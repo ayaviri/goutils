40ea857db42148a805e65720be4cff62d4f03c03
ayaviri <joshuayaviri@gmail.com>
2024-10-25 19:17:50 -0400

    Adding logs to see if the middleware actually mutates the incoming request

--- http/middleware.go
+++ http/middleware.go
@@ -1,10 +1,15 @@
 package http
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func StripQueryString(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.URL.RawQuery = ""
+
+		fmt.Println(r.URL)
 
 		next.ServeHTTP(w, r)
 	})
